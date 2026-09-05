3b896ae2685b6be265b08ccbd5751b2840d5c675
Amr Khaled <143969993+amr0khaled@users.noreply.github.com>
2026-09-05 17:52:37 +0300

    Add solution for Two Sum - LeetCode

--- Two_Sum_LeetCode/solution.js
+++ Two_Sum_LeetCode/solution.js
@@ -1,17 +1,16 @@
-                        i += 99;
-                        first += 99;
-                    } else if (size - i > 50) {
-                        i += 49;
-                    } else if (size - i > 100) {
-                        first += 199;
-                        first += 49;
-                    }
-                        i += 199;
-                    } else if (size - i > 200) {
-                        first += 499;
-                        i += 499;
-                    if (size - i > 500) {
-                if (failed > 18) {
-                failed++;
-            } else {
-                found = 1;
+                    first = -1;
+            if (i == size) {
+                if (first == size) {
+                break;
+            }
+            if (found) {
+            cout << i << " " << first << " " << last << endl;
+        for (int i = 1; i < (size + 1); i++) {
+        int size = nums.size();
+        int failed = 0;
+        int last = 0;
+        int first = 0;
+        int found = 0;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+class Solution {
