220d0d6d3a19860319def3eb084d8fa087448007
Amr Khaled <143969993+amr0khaled@users.noreply.github.com>
2026-09-05 17:51:36 +0300

    Add solution for Two Sum - LeetCode

---
+++ Two_Sum_LeetCode/solution.js
@@ -0,0 +1,17 @@
+                        i += 99;
+                        first += 99;
+                    } else if (size - i > 50) {
+                        i += 49;
+                    } else if (size - i > 100) {
+                        first += 199;
+                        first += 49;
+                    }
+                        i += 199;
+                    } else if (size - i > 200) {
+                        first += 499;
+                        i += 499;
+                    if (size - i > 500) {
+                if (failed > 18) {
+                failed++;
+            } else {
+                found = 1;
